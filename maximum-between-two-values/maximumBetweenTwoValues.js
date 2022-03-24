395be7dd3367830b12ff429608ac6e56f95fa821
mardenmoura <moura.dev@outlook.com>
2022-03-24 05:48:01 -0300

    First commit

---
+++ maximum-between-two-values/maximumBetweenTwoValues.js
@@ -0,0 +1,8 @@
+// Write the function that takes 2 numbers and returns the largest of them.
+
+let higherValue = max (10,2)
+console.log(higherValue)
+
+function max(number1, number2){
+    return number1 > number2 ? number1: number2
+}
